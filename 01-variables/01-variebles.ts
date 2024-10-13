e7083a7fdfd82d050d88dab31841c51c552035e2
Grasmeyster87 <grasmeyster@gmail.com>
2024-10-13 14:33:15 +0300

    lesson 1 35 minutes

---
+++ 01-variables/01-variebles.ts
@@ -0,0 +1,10 @@
+// Визначення змінної з ім'ям message1
+// та значенням Hello без явної вказівки типу
+let message1 = 'Hello!';
+
+// Визначення змінної з ім'ям message2
+// типу string та присвоєння змінної значення Hello!
+let message2: string = 'Hello';
+
+// Створення константи
+const message3: string = 'Hello';
